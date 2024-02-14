d9561dfc2648644e6acc4b7fb028e986f2d04a3b
Charles Maina Mwangi <38659056+MainaMwangiy@users.noreply.github.com>
2024-02-15 00:38:41 +0300

    Dev/maina/#12 make the app pwa (#13)
    * initial - remove unused import (#8)
    
    * #12 - make the app installable by making it pwa

--- src/index.js
+++ src/index.js
@@ -17,3 +17,13 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
